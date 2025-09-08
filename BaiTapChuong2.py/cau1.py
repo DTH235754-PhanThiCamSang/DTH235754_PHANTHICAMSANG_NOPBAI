14e83ad51faf3370c2e3f8f4fd8fda689e32a2d5
Sang Phan <sang_dth235754@student.agu.edu.vn>
2025-09-08 08:36:33 +0700

    bai_chuong2

---
+++ BaiTapChuong2.py/cau1.py
@@ -0,0 +1,9 @@
+import math
+try:
+    r=float(input ("Mời bạn nhập bán kính hình tròn: "))
+    cv=2*math.pi*r
+    dt=r**2
+    print("chu ci =",cv)
+    print("diện tích=",dt)
+except:
+    print("Lỗi rồi!")
