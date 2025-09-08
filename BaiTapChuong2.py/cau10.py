bd6463202f9ac0e05922931b652dd278a22f2c24
Sang Phan <sang_dth235754@student.agu.edu.vn>
2025-09-08 09:10:02 +0700

    bai10_chuong2

---
+++ BaiTapChuong2.py/cau10.py
@@ -0,0 +1,9 @@
+# Viết ngắn gọn lại các lệnh sử dụng toán tử gán kết hợp
+
+x += 1        # (a)
+x /= 2        # (b)
+x -= 1        # (c)
+x += y        # (d)
+x -= (y + 7)  # (e)
+x *= 2        # (f)
+number_of_closed_cases += 2 * ncc  # (g)
