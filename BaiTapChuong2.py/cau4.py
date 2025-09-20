36bfe4d085bd9d6cf21e5b529c7fd76d8a7214da
Sang Phan <sang_dth235754@student.agu.edu.vn>
2025-09-20 14:28:25 +0700

    uploat bai4

--- BaiTapChuong2.py/cau4.py
+++ BaiTapChuong2.py/cau4.py
@@ -2,35 +2,35 @@
 Câu 4: Python hỗ trợ những kiểu dữ liệu cơ bản nào?
 Python hỗ trợ nhiều kiểu dữ liệu cơ bản như:
 
-Số (Number):
++ Số (Number):
 
-int: Số nguyên (e.g., 1, -5)
+int: Số nguyên (VD: 1, -5)
 
-float: Số thực (e.g., 3.14, -0.5)
+float: Số thực (VD: 3.14, -0.5)
 
-complex: Số phức (e.g., 2 + 3j)
+complex: Số phức (VD: 2 + 3j)
 
-Chuỗi (String):
++ Chuỗi (String):
 
 Dùng để lưu văn bản, ký tự. VD: 'hello', "123"
 
-Boolean (bool):
++ Boolean (bool):
 
 Chỉ có hai giá trị: True và False
 
-Danh sách (list):
++ Danh sách (list):
 
 Danh sách có thể thay đổi, chứa nhiều kiểu dữ liệu. VD: [1, 2, 'a']
 
-Tuple (tuple):
++ Tuple (tuple):
 
 Giống list nhưng bất biến (không thay đổi). VD: (1, 2, 3)
 
-Tập hợp (set):
++ Tập hợp (set):
 
 Tập hợp không chứa phần tử trùng lặp. VD: {1, 2, 3}
 
-Từ điển (dict):
++ Từ điển (dict):
 
 Dạng key–value. VD: {'name': 'SAN', 'age': 20}
 '''
