abcf057a709f15c31679dd0b84d6fa6b349a6abc
Sang Phan <sang_dth235754@student.agu.edu.vn>
2025-10-02 15:45:21 +0700

    bai16

---
+++ BaiTapChuong3.py/bai16.py
@@ -0,0 +1,13 @@
+#cho biet bao nhieu dau sao duoc in ra
+'''
+for i in range(20,100,5):
+    print("*", end="")
+print()
+
+
+giai thich
+# Vòng lặp for i in range(20, 100, 5):
+# Bắt đầu từ 20, kết thúc trước 100, bước nhảy 5    
+# Các giá trị của i sẽ là: 20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80, 85, 90, 95
+# Tổng cộng có 16 giá trị ⟹ In ra 16 dấu *
+'''
