25d4da88b235aac292830936c8ca64acd435dc9c
Sang Phan <sang_dth235754@student.agu.edu.vn>
2025-10-02 15:31:39 +0700

    ket qua xuat ra man hinh

---
+++ BaiTapChuong3.py/bai5.py
@@ -0,0 +1,102 @@
+#cho biet ket qua xuat ra man hinh
+
+print("(a)")
+i = 3
+j = 5
+k = 7
+
+if i < j:
+    if j < k:
+        i = j
+    else:
+        j = k
+else:
+    if j > k:
+        j = i
+    else:
+        i = k
+print("i =",i,"j =",j,"k =",k)
+
+
+print("(b)")
+i=3
+j=7
+k=5
+
+if i < j:
+    if j < k:
+        i = j
+    else:
+        j = k
+else:
+    if j > k:
+        j = i
+    else:
+        i = k
+print("i =",i,"j =",j,"k =",k)
+
+print("(c)")
+i=5
+j=3
+k=7
+if i < j:
+    if j < k:
+        i = j
+    else:
+        j = k
+else:
+    if j > k:
+        j = i
+    else:
+        i = k
+print("i =",i,"j =",j,"k =",k)
+
+print("(d)")
+i=5
+j=7
+k=3
+
+if i < j:
+    if j < k:
+        i = j
+    else:
+        j = k
+else:
+    if j > k:
+        j = i
+    else:
+        i = k
+print("i =",i,"j =",j,"k =",k)
+
+print("(e)")
+i=7
+j=3
+k=5
+if i < j:
+    if j < k:
+        i = j
+    else:
+        j = k
+else:
+    if j > k:
+        j = i
+    else:
+        i = k
+print("i =",i,"j =",j,"k =",k)
+
+print("(f)")
+i=7
+j=5
+k=3
+if i < j:
+    if j < k:
+        i = j
+    else:
+        j = k
+else:
+    if j > k:
+        j = i
+    else:
+        i = k
+print("i =",i,"j =",j,"k =",k)
+
