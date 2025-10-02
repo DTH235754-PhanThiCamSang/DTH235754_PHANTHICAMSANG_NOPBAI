6c07858bed9da1cde672ad864fecd5f65046458e
Sang Phan <sang_dth235754@student.agu.edu.vn>
2025-10-02 15:37:15 +0700

    bang cuu chuong

---
+++ BaiTapChuong3.py/bai12.py
@@ -0,0 +1,8 @@
+#xuat bang cuu chuong
+print("Chuong trinh xuat bang cuu chuong")
+for i in range(1,11):
+    for j in range(2,10):
+        line ="{0}*{1:>2}={2:>2}".format(j,i,j*i)
+        print(line, end="\t")
+    print()
+       
