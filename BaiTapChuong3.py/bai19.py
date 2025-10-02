09a893b62042c6cd44aad2008f2529dcfe07eed9
Sang Phan <sang_dth235754@student.agu.edu.vn>
2025-10-02 16:13:12 +0700

    Tính giá trị biểu thức S

---
+++ BaiTapChuong3.py/bai19.py
@@ -0,0 +1,13 @@
+import math
+n = int(input("Nhap n: "))
+x = int(input("Nhap x: "))
+def series_s(x: int, n: int) -> float:
+    if n < 0:
+        raise ValueError("n phải là số nguyên không âm")
+    term = x          # T0
+    s = term
+    for k in range(1, n + 1):
+        term *= (x * x) / ( (2*k) * (2*k + 1) )
+        s += term
+    return s
+print(f"S({x}, {n}) = {series_s(x, n)}")
